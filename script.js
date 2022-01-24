1c21267ebb109395a342dfc759d693f3ac6208c8
FlavioLAMartins <flavio_martins11@hotmail.com>
2022-01-24 13:35:46 -0300

    add loader

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ typeWriter(title)
 
 const escreva = document.getElementById('text-copy')
 
+let loader = document.querySelector('.page-loading')
+let pageDecode = document.querySelector('main')
+let load = setInterval(animate, 50)
+let c = 0
+function animate() {
+  if (c == 50) {
+    loader.parentElement.removeChild(loader)
+    document.querySelector('main').style.visibility = 'visible'
+    clearInterval(load)
+  } else {
+    c++
+  }
+}
+
 //For creating the encoding
 function code() {
   let txt = document.getElementById('input-text')
